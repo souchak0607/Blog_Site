7f4dcd9d2049b907fc6633207a751e9a4a96f48d
chakraborty.soumyajit.0607@gmail.com <chakraborty.soumyajit.0607@gmail.com>
2024-05-20 22:52:20 +0530

    After making Blog model

--- myproject/blogs/models.py
+++ myproject/blogs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True)
