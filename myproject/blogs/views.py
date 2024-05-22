b599e8fc29cd52743ced3f3365e1edce07ab6185
chakraborty.soumyajit.0607@gmail.com <chakraborty.soumyajit.0607@gmail.com>
2024-05-23 00:59:53 +0530

    Form for New Post

--- myproject/blogs/views.py
+++ myproject/blogs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blog
 from django.contrib.auth.decorators import login_required
+from . import forms
 # Create your views here.
 
 def blog_list(request):
@@ -13,6 +14,15 @@ def blog_page(request, slug):
 
 @login_required(login_url="/users/login")
 def blog_new(request):
-    return render(request, 'blogs/blog_new.html')
+    if request.method == "POST":
+        form = forms.CreateBlog(request.POST, request.FILES)
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return redirect('blogs:list')
+    else:
+        form = forms.CreateBlog()
+    return render(request, 'blogs/blog_new.html', { 'form':form })
 
 
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dn#h1d&mw#)v5vdkk+x%b@fs^f8le9#z=%0i!l&zyd-&+l2h@f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
 # Application definition
@@ -118,14 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATIC_ROOT = BASE_DIR / 'assests'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
     ]
 
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
